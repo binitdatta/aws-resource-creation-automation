9664e46ffbfd04371d86b2ea506f332a4faf1608
Binit Datta <bdatta@westmonroepartners.com>
2017-07-07 11:08:10 -0500

    AWS EC2 Instance Creation Scripts

--- ec2_instance_creation.sh
+++ ec2_instance_creation.sh
@@ -1,4 +1,1 @@
-/home/ec2-user/.local/bin/aws ec2 run-instances --image-id ami-b55a51cc --iam-instance-profile Name=ec2Instances \  --instance-type t2.micro --count 1 --key-name eCommerceSampleKeyPair \
---user-data $WORKSPACE/aws_ec2_user_data_script.txt \
---subnet-id subnet-1a8afe43 --security-group-ids sg-ef6abe89 \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=BinitAWSCLIEC2Instance}]' 
+/home/ec2-user/.local/bin/aws ec2 run-instances --image-id ami-b55a51cc --iam-instance-profile Name=ec2Instances --instance-type t2.micro --count 1 --key-name eCommerceSampleKeyPair --user-data $WORKSPACE/aws_ec2_user_data_script.txt --subnet-id subnet-1a8afe43 --security-group-ids sg-ef6abe89 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=BinitAWSCLIEC2Instance}]'
